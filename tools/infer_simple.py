7fff25b2f858aed91e9762dd79e57024bde8cf98
Coder_Hong <fangyh09@gmail.com>
2018-12-11 22:45:41 +0800

    Update infer_simple.py

--- tools/infer_simple.py
+++ tools/infer_simple.py
@@ -113,6 +113,14 @@ def parse_args():
     parser.add_argument(
         'im_or_folder', help='image or folder of images', default=None
     )
+    parser.add_argument(
+        '--inputlist',
+        type=str
+    )
+    parser.add_argument(
+        '--basepath',
+        type=str
+    )
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(1)
@@ -135,16 +143,36 @@ def main(args):
     model = infer_engine.initialize_model_from_cfg(args.weights)
     dummy_coco_dataset = dummy_datasets.get_coco_dataset()
 
-    if os.path.isdir(args.im_or_folder):
-        im_list = glob.iglob(args.im_or_folder + '/*.' + args.image_ext)
-    else:
-        im_list = [args.im_or_folder]
+    # comment old file
+    # if os.path.isdir(args.im_or_folder):
+        # im_list = glob.iglob(args.im_or_folder + '/*.' + args.image_ext)
+    # else:
+        # im_list = [args.im_or_folder]
+    # use inputlist
+    if args.inputlist:
+        # print(">>> args.inputlist") 
+        def read_file(fname):
+            with open(fname) as f:
+                content = f.readlines()
+                content = [x.strip() for x in content]
+            return content
+        imgnames = read_file(args.inputlist)
+        # print(type(imgnames))
+        # print(imgnames)
+        im_list = imgnames
+    # add base path
+    if args.basepath:
+        im_list = [os.path.join(args.basepath, x) for x in im_list]
 
+    result_all = []
     for i, im_name in enumerate(im_list):
+        print(">>> im_name", im_name)
         out_name = os.path.join(
             args.output_dir, '{}'.format(os.path.basename(im_name) + '.' + args.output_ext)
         )
         logger.info('Processing {} -> {}'.format(im_name, out_name))
+        if not os.path.exists(im_name):
+            raise ValueError("image name not exists,", im_name)
         im = cv2.imread(im_name)
         timers = defaultdict(Timer)
         t = time.time()
@@ -161,21 +189,59 @@ def main(args):
                 'rest (caches and auto-tuning need to warm up)'
             )
 
-        vis_utils.vis_one_image(
-            im[:, :, ::-1],  # BGR -> RGB for visualization
-            im_name,
-            args.output_dir,
-            cls_boxes,
-            cls_segms,
-            cls_keyps,
-            dataset=dummy_coco_dataset,
-            box_alpha=0.3,
-            show_class=True,
-            thresh=args.thresh,
-            kp_thresh=args.kp_thresh,
-            ext=args.output_ext,
-            out_when_no_box=args.out_when_no_box
-        )
+        import numpy as np
+        np.set_printoptions(suppress=True)
+        # print(type(cls_keyps))
+        # print("len", len(cls_keyps))
+        # print(type(cls_keyps[0]))
+        # print("cls_keyps[0] len", len(cls_keyps[0]))
+        # print("cls_keyps[1] len", len(cls_keyps[1]))
+        cls_keyps1 = cls_keyps[1]
+        cls_keyps1 = np.array(cls_keyps1) 
+        pts = cls_keyps1[0][:4, :]
+        pts = np.transpose(pts, (1, 0))
+        pts_lt = pts.tolist()
+        # print("pts_lt", pts_lt)
+        # print("pts.shape", pts.shape)
+        result_all.append(
+                {
+                    "image_id": im_name,
+                    "keypoint": pts_lt
+                    }
+                )
+        """
+        n x 4 x 17
+        """
+        # cls_keyps1 = np.reshape(cls_keyps1, (-1,3))
+        # print("cls_keyps1.reshape", cls_keyps1.shape)
+        # print("one.shape", cls_keyps1[0].shape)
+        # print("cls_keyps1", cls_keyps1[0])
+        # print("type cls_keyps1", type(cls_keyps1))
+        # print(cls_keyps1)
+
+        # vis_utils.vis_one_image(
+            # im[:, :, ::-1],  # BGR -> RGB for visualization
+            # im_name,
+            # args.output_dir,
+            # cls_boxes,
+            # cls_segms,
+            # cls_keyps,
+            # dataset=dummy_coco_dataset,
+            # box_alpha=0.3,
+            # show_class=True,
+            # thresh=args.thresh,
+            # kp_thresh=args.kp_thresh,
+            # ext=args.output_ext,
+            # out_when_no_box=args.out_when_no_box
+        # )
+
+    import json
+    def write_json(fname, data):
+        with open(fname, 'w') as outfile:
+            json.dump(data, outfile) 
+    write_json("maskrcnn_h36m.json", result_all)
+    
+
 
 
 if __name__ == '__main__':
